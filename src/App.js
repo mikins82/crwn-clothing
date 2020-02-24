32c7d25a547da14323b402eb543a762750461bd7
miroslav.jugovic <miroslav.jugovic@nttdata.ro>
2020-02-24 16:52:04 +0100

    implemented firebase utils, included ability to store authenticated users into our firestore database

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Route, Switch } from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Header from './components/header/header.component';
-import SignInAndSignupPage from './pages/sign-in-and-sign-out/sign-in-and-sign-out.component';
+import SignInAndSignUpPage from './pages/sign-in-and-sign-out/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
@@ -31,8 +31,12 @@ class App extends React.Component {
               ...snapShot.data()
             }
           });
+
+          console.log(this.state);
         });
       }
+
+      this.setState({ currentUser: userAuth });
     });
   }
 
@@ -47,7 +51,7 @@ class App extends React.Component {
         <Switch>
           <Route exact path='/' component={HomePage}/>
           <Route exact path='/shop' component={ShopPage}/>
-          <Route exact path='/signin' component={SignInAndSignupPage}/>
+          <Route exact path='/signin' component={SignInAndSignUpPage}/>
         </Switch>
       </div>
     );
--- src/pages/sign-in-and-sign-out/sign-in-and-sign-out.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import './sign-in-and-sign-out.styles.scss';
-import SignIn from '../../components/sign-in/sign-in.component';
-
-const SignInAndSignupPage = () => (
-  <div className='sign-in-and-sign-out'><SignIn></SignIn></div>
-)
-
-export default SignInAndSignupPage;
