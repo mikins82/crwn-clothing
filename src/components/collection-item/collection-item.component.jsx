d22598ade623dce16ed7f11b5316c58a2d0c8971
miroslav.jugovic <miroslav.jugovic@nttdata.ro>
2020-03-01 17:55:16 +0100

    moved our directory and shop data into its own reducer, created respective selectors and updated corresponding components with new redux flow for directory/shop

--- src/components/collection-item/collection-item.component.jsx
+++ src/components/collection-item/collection-item.component.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import { connect } from 'react-redux';
+
+import CustomButton from '../custom-button/custom-button.component';
+import { addItem } from '../../redux/cart/cart.actions';
 
 import './collection-item.styles.scss';
-import CustomButton from '../custom-button/custom-button.component';
-import { connect } from 'react-redux';
-import { addItem } from '../../redux/cart/cart.actions';
 
 const CollectionItem = ({ item, addItem }) => {
   const { name, price, imageUrl } = item;
+
   return (
     <div className='collection-item'>
       <div
@@ -14,8 +16,7 @@ const CollectionItem = ({ item, addItem }) => {
         style={{
           backgroundImage: `url(${imageUrl})`
         }}
-      >
-      </div>
+      />
       <div className='collection-footer'>
         <span className='name'>{name}</span>
         <span className='price'>{price}</span>
@@ -24,11 +25,14 @@ const CollectionItem = ({ item, addItem }) => {
         Add to cart
       </CustomButton>
     </div>
-  )
+  );
 };
 
 const mapDispatchToProps = dispatch => ({
   addItem: item => dispatch(addItem(item))
 });
 
-export default connect(null, mapDispatchToProps)(CollectionItem);
+export default connect(
+  null,
+  mapDispatchToProps
+)(CollectionItem);
