2770690dac29df904c2d00e9eea0fca4c256fa89
miroslav.jugovic <miroslav.jugovic@nttdata.ro>
2020-03-11 10:22:19 +0100

    Added context api and context provider. Changed from some redux parts

---
+++ src/contexts/current-user/current-user.context.js
@@ -0,0 +1,5 @@
+import { createContext } from 'react';
+
+const CurrentUserContext = createContext(undefined); // initial value
+
+export default CurrentUserContext;
--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { connect } from 'react-redux';
-
-import CollectionItem from '../../components/collection-item/collection-item.component';
-
-import { selectCollection } from '../../redux/shop/shop.selectors';
+import React, { useContext } from 'react';
 
 import './collection.styles.scss';
 
-const CollectionPage = ({ collection }) => {
+import CollectionItem from '../../components/collection-item/collection-item.component';
+import CollectionsContext from '../../contexts/collections/collections.context';
+
+const CollectionPage = ({ match }) => {
+  const collections = useContext(CollectionsContext);
+  const collection = collections[match.params.collectionId];
   const { title, items } = collection;
   return (
     <div className='collection-page'>
@@ -21,8 +21,4 @@ const CollectionPage = ({ collection }) => {
   );
 };
 
-const mapStateToProps = (state, ownProps) => ({
-  collection: selectCollection(ownProps.match.params.collectionId)(state)
-});
-
-export default connect(mapStateToProps)(CollectionPage);
+export default CollectionPage;
